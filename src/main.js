d82522bb9c230e7c88d1a5a520d1d85b575f06e4
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-07-31 15:00:24 +0100

    completed the aritGeo function in main.js

---
+++ src/main.js
@@ -0,0 +1,70 @@
+module.exports = {
+    
+    aritGeo: (inputArray) => {
+
+        // tests invalid input - inputs that are not arrays
+        if(!(inputArray instanceof Array)) {
+            throw new Error("input should be an array");
+        }
+
+        // tests if input array is not empty
+        if (inputArray.length === 0) {
+            return 0;
+        }
+
+        // input array cannot be tested as either arithmetic or geometric 
+        // if number of elements are less than 3
+        // function should return -1 in this case stating inputArray is neither
+        if (inputArray.length === 1 || inputArray.length === -2) {
+            return -1;
+        }
+
+        // tests if elements of input are all numbers
+        if (inputArray.some(isNaN)) {
+            throw new Error("input array contains invalid elements");
+        }
+
+        if (testArithmetic(inputArray)) {
+            return "Arithmetic";
+        }
+
+        if (testGeometric(inputArray)) {
+            return "Geometric";
+        }
+
+        // default case when inputArray is neither
+        return -1;
+
+    },
+}
+
+function testArithmetic(inputArray) {
+    let commonDifference = inputArray[1] - inputArray[0];            
+    let arrayOfDifs = [];
+
+    for(let i = 1; i < inputArray.length; i++) {
+        let diff = inputArray[i+1] - inputArray[i];
+        arrayOfDifs.push(diff);
+    }    
+    arrayOfDifs = arrayOfDifs.splice(0, arrayOfDifs.length - 1);            
+    if(arrayOfDifs.every((element) => { return element === commonDifference;})) { 
+        return true
+    }
+    return false;
+}
+
+function testGeometric(inputArray) {
+    let commonRatio = inputArray[1]/inputArray[0];            
+    let arrayOfRatios = [];
+
+    for(let i = 1; i < inputArray.length; i++) {
+        let ratio = inputArray[i+1]/inputArray[i];
+        arrayOfRatios.push(ratio);
+    }    
+    arrayOfRatios = arrayOfRatios.splice(0, arrayOfRatios.length - 1);
+
+    if(arrayOfRatios.every((element) => { return element === commonRatio;})) { 
+        return true
+    }
+    return false;
+}
--- tests/tests.js
+++ tests/tests.js
@@ -1,31 +1,19 @@
 const assert = require('chai').assert;
+const expect = require('chai').expect;
 const aritGeo = require('../src/main.js');
 
 describe("aritGeo", () => {
 
     describe("handle invalid input", () => {
         it("should return undefined for a string", () => {
-           assert.equal(aritGeo.aritGeo(" ", 'undefined')); 
+           assert.equal(aritGeo.aritGeo(" ", "undefined")); 
         });
 
         it("should return 0 for an empty array", () => {
            assert.equal(aritGeo.aritGeo([], 0)); 
         });
-
-        it("should return undefined for an object", () => {
-           assert.equal(aritGeo.aritGeo({}, 'undefined')); 
-        });
-
         
     });
 
-    describe("handle invalid input", () => {
-        it("should return undefined for a string", () => {
-           assert.equal(aritGeo.aritGeo(" ", 'undefined')); 
-        });
-
-        it("should return undefined for a string", () => {
-           assert.equal(aritGeo.aritGeo(" ", 'undefined')); 
-        });    
-    });
+    
 });
